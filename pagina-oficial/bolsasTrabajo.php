6a45b3d70393d2dbfde45f1a0eca3f9b6091127d
Aquiinovargas <155691754+Aquiinovargas@users.noreply.github.com>
2024-01-04 18:30:15 -0600

    Add files via upload

---
+++ pagina-oficial/bolsasTrabajo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bolsas de Trabajo</title>
+    <?php include('com-enlaces-head.php'); ?>
+</head>
+<body>
+    <header>
+        <?php include('com-menu.php');?>
+    </header>
+</body>
+</html>
---
+++ pagina-oficial/com-enlaces-head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="shortcut icon" href="assets/images/Logo/indigena.png" type="image/x-icon">
+<script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
